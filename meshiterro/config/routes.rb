c77658449cf6fc019d23baf0e8f3cba5de644aef
takapamui <takapamukyary0129@gmail.com>
2021-04-10 13:29:59 +0000

    [Finish]4-6

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'user/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 devise_for :users
 root 'post_images#index'
@@ -7,4 +8,5 @@ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
 resource :favorites, only: [:create, :destroy]
 resources :post_comments, only: [:create, :destroy]
 end
+resources :users, only: [:show, :edit, :update]
 end
